2084effde661b9f5fdeef68cc1cba761f6567e95
tywenrick3 <tew287@nyu.edu>
2020-02-25 18:00:14 -0500

    changes

--- week_05/random.js
+++ week_05/random.js
@@ -1,3 +1,20 @@
 var colors = ['#008000', '#D3D3D3'];
-var random_color = colors[Math.floor(Math.random() * colors.length)];
-print(random_color);
+var green = '#008000';
+var grey = '#D3D3D3';
+
+var random_color = false;
+
+if (colors[Math.floor(Math.random() * colors.length)] == 1){
+    random_color = true;
+}else{
+    random_color = false;
+}
+console.log(random_color);
+
+//document.getElementById("doIt").style.backgroundColor = random_color;
+
+//if (random_color == 0){
+  //  document.getElementById("dont").style.backgroundColor = random_color;
+//} else if (random_color == 1){
+    //document.getElementById("dont").style.backgroundColor = random_color;
+//}
